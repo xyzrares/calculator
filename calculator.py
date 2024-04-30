e240bddaa2b56a4db43ac3d755439a12dc48b8e4
xyzrares <xyz.raresmorariu@gmail.com>
2024-04-30 22:18:45 +0300

    Implemented the arithmetic functions of the calculato

--- calculator.py
+++ calculator.py
@@ -2,26 +2,23 @@
 # Use try blocks to handle errors like division by zero.
 
 def addition(a, b):
-    c = a + b 
-    return c 
+    return a + b 
+    
 
 def substraction(a, b):
-    c = a - b
-    return c
+    return a - b
 
 def multiplication(a, b):
-    c = a * b
-    return c
+    return a * b
 
 def power(a, b):
-    c = a ** b
-    return c
+    return a ** b
 
 def division(a, b):
     try:
         c = a / b
         if b == 0:
-            raise ValueError("You cannot divide by 0.")
+            raise ValueError("You cannot divide by 0, cowboy.")
     except ValueError as e: 
         print(e)
     return c
@@ -30,7 +27,7 @@ def floor_division(a, b):
     try:
         c = a // b
         if b == 0:
-            raise ValueError("You cannot divide by 0.")
+            raise ValueError("You cannot divide by 0, cowboy.")
     except ValueError as e: 
         print(e)
     return c
@@ -39,8 +36,43 @@ def modulus(a, b):
     try:
         c = a % b
         if b == 0:
-            raise ValueError("You cannot divide by 0.")
+            raise ValueError("You cannot divide by 0, cowboy.")
     except ValueError as e: 
         print(e)
     return c
 
+def main():
+    user = input("How should I call you, big boi?: ")
+    print(f"Well, hello, there, {user}!")
+    
+    answer = input("Do ya wanna watch me solve some quantum physics?(Y/N): ")
+    if answer.lower() == y:
+        print("Well, too bad, cowboy! This ain't nun but a calculator. It ain't much but it's honest work, y'know...")
+    elif answer.lower() == n:
+        print("Didn't wanna show it to ya, anyways...")
+        
+    a = input("What's the first number, cowboy?: ")
+    b = input("Ya need a second one if you want to use me: ")
+    operation = input("What do you want to know, big boi?: ")
+    if operation.lower() == "addition" or operation.lower() == "+":
+        c = addition(a, b)
+        return print(f"Your result is: {c}")
+    elif operation.lower() == "substraction" or operation.lower() == "-":
+        c = substraction(a, b)
+        return print(f"Your result is: {c}")
+    elif operation.lower() == "multiplication" or operation.lower() == "*":
+        c = multiplication(a, b)
+        return print(f"Your result is: {c}")
+    elif operation.lower() == "power" or operation.lower() == "**":
+        c = power(a, b)
+        return print(f"Your result is: {c}")
+    elif operation.lower() == "division" or operation.lower() == "/":
+        c = division(a, b)
+        return print(f"Your result is: {c}")
+    elif operation.lower() == "floor division" or operation.lower() == "//":
+        c = floor_division(a, b)
+        return print(f"Your result is: {c}")
+    elif operation.lower() == "modulus" or operation.lower() == "%":
+        c = modulus(a, b)
+        return print(f"Your result is: {c}")
+    
