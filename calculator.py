25869765256158982b78f0e16e7a736958ff82a3
xyzrares <xyz.raresmorariu@gmail.com>
2024-04-30 22:41:40 +0300

    Improved the main function

--- calculator.py
+++ calculator.py
@@ -35,29 +35,38 @@ def main():
         print("Well, too bad, cowboy! This ain't nun but a calculator. It ain't much but it's honest work, y'know...")
     elif answer.lower() == n:
         print("Didn't wanna show it to ya, anyways...")
-        
-    a = float(input("What's the first number, cowboy?: "))
-    b = float(input("Ya need a second one if you want to use me: "))
-    operation = input("What do you want to know, big boi?: ")
-    if operation.lower() == "addition" or operation.lower() == "+":
-        c = addition(a, b)
-        return print(f"Your result is: {c}")
-    elif operation.lower() == "substraction" or operation.lower() == "-":
-        c = substraction(a, b)
-        return print(f"Your result is: {c}")
-    elif operation.lower() == "multiplication" or operation.lower() == "*":
-        c = multiplication(a, b)
-        return print(f"Your result is: {c}")
-    elif operation.lower() == "power" or operation.lower() == "**":
-        c = power(a, b)
-        return print(f"Your result is: {c}")
-    elif operation.lower() == "division" or operation.lower() == "/":
-        c = division(a, b)
-        return print(f"Your result is: {c}")
-    elif operation.lower() == "floor division" or operation.lower() == "//":
-        c = floor_division(a, b)
-        return print(f"Your result is: {c}")
-    elif operation.lower() == "modulus" or operation.lower() == "%":
-        c = modulus(a, b)
-        return print(f"Your result is: {c}")
+        for i in range(3, 0, -1):
+            print(f"The program will closed in {i} seconds...")
+            
+    while True:         
+        a = float(input("What's the first number, cowboy?: "))
+        b = float(input("Ya need a second one if you want to use me: "))
+        operation = input("What do you want to know, big boi? [addition(+); substraction(-); multiplication(*); power(**); division(/); floor division(//); modulus(%)]: ")
+        if operation.lower() == "addition" or operation.lower() == "+":
+            c = addition(a, b)
+        elif operation.lower() == "substraction" or operation.lower() == "-":
+            c = substraction(a, b)
+        elif operation.lower() == "multiplication" or operation.lower() == "*":
+            c = multiplication(a, b)
+        elif operation.lower() == "power" or operation.lower() == "**":
+            c = power(a, b)
+        elif operation.lower() == "division" or operation.lower() == "/":
+            c = division(a, b)
+        elif operation.lower() == "floor division" or operation.lower() == "//":
+            c = floor_division(a, b)
+        elif operation.lower() == "modulus" or operation.lower() == "%":
+            c = modulus(a, b)
+        else:
+            print("Invlaid operation, cowboy!")
+            continue
+    except ValueError as e: 
+        print("Invalid input:", e)
+        continue
+    else: 
+        print(f"The result is: {c}")
     
+    if input("Do ya wanna use me again? (Y/N): ").lower() != "y":
+        break
+    
+if __name__ == "__main__":
+    main()
