e0d9c2d4bb05999f13b39ccb4129a66cf5cda98d
xyzrares <xyz.raresmorariu@gmail.com>
2024-04-30 22:44:27 +0300

    Corrected the indentation and the try block as it was missing

--- calculator.py
+++ calculator.py
@@ -38,35 +38,36 @@ def main():
         for i in range(3, 0, -1):
             print(f"The program will closed in {i} seconds...")
             
-    while True:         
-        a = float(input("What's the first number, cowboy?: "))
-        b = float(input("Ya need a second one if you want to use me: "))
-        operation = input("What do you want to know, big boi? [addition(+); substraction(-); multiplication(*); power(**); division(/); floor division(//); modulus(%)]: ")
-        if operation.lower() == "addition" or operation.lower() == "+":
-            c = addition(a, b)
-        elif operation.lower() == "substraction" or operation.lower() == "-":
-            c = substraction(a, b)
-        elif operation.lower() == "multiplication" or operation.lower() == "*":
-            c = multiplication(a, b)
-        elif operation.lower() == "power" or operation.lower() == "**":
-            c = power(a, b)
-        elif operation.lower() == "division" or operation.lower() == "/":
-            c = division(a, b)
-        elif operation.lower() == "floor division" or operation.lower() == "//":
-            c = floor_division(a, b)
-        elif operation.lower() == "modulus" or operation.lower() == "%":
-            c = modulus(a, b)
-        else:
-            print("Invlaid operation, cowboy!")
+    while True: 
+        try:         
+            a = float(input("What's the first number, cowboy?: "))
+            b = float(input("Ya need a second one if you want to use me: "))
+            operation = input("What do you want to know, big boi? [addition(+); substraction(-); multiplication(*); power(**); division(/); floor division(//); modulus(%)]: ")
+            if operation.lower() == "addition" or operation.lower() == "+":
+                c = addition(a, b)
+            elif operation.lower() == "substraction" or operation.lower() == "-":
+                c = substraction(a, b)
+            elif operation.lower() == "multiplication" or operation.lower() == "*":
+                c = multiplication(a, b)
+            elif operation.lower() == "power" or operation.lower() == "**":
+                c = power(a, b)
+            elif operation.lower() == "division" or operation.lower() == "/":
+                c = division(a, b)
+            elif operation.lower() == "floor division" or operation.lower() == "//":
+                c = floor_division(a, b)
+            elif operation.lower() == "modulus" or operation.lower() == "%":
+                c = modulus(a, b)
+            else:
+                print("Invlaid operation, cowboy!")
+                continue
+        except ValueError as e: 
+            print("Invalid input:", e)
             continue
-    except ValueError as e: 
-        print("Invalid input:", e)
-        continue
-    else: 
-        print(f"The result is: {c}")
+        else: 
+            print(f"The result is: {c}")
     
-    if input("Do ya wanna use me again? (Y/N): ").lower() != "y":
-        break
+        if input("Do ya wanna use me again? (Y/N): ").lower() != "y":
+            break
     
 if __name__ == "__main__":
     main()
